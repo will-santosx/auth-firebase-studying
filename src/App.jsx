87aebd451450bf541256add2364689afb366e764
Willians Santos <taldowill29@gmail.com>
2023-07-29 18:07:53 -0400

    :sparkles: feat: Interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,132 @@
+import {
+	FaEnvelope,
+	FaLock,
+	FaUser,
+	FaGoogle,
+	FaSignInAlt,
+} from "react-icons/fa";
+import { useState } from "react";
+import { BeatLoader } from "react-spinners";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
-	return <main></main>;
+	const [nameInput, setNameInput] = useState("");
+	const [emailInput, setSetEmailInput] = useState("");
+	const [passwordInput, setPasswordInput] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [notified, setNotified] = useState(true);
+
+	function showNotification(text, type) {
+		if (text && text !== "") {
+			switch (type) {
+				case "success":
+					toast.success(`${text}`);
+					break;
+				case "error":
+					toast.error(`${text}`);
+					break;
+				default:
+					toast.info(`${text}`);
+			}
+		}
+	}
+
+	if (loading) {
+		return (
+			<main className="w-screen h-screen flex flex-col justify-center items-center">
+				<BeatLoader size={30} color="#121063" />
+			</main>
+		);
+	}
+
+	return (
+		<main className="w-screen h-screen flex flex-col font-mono">
+			<ToastContainer
+				position="bottom-left"
+				autoClose={2000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover={false}
+				theme="dark"
+			/>
+			<div className="bg-midnight p-2 h-6 flex items-center justify-center">
+				<span className="tracking-wider text-light">
+					aprendendo a utilizar os métodos de autenticação do firebase.
+				</span>
+			</div>
+			<div className="w-full h-full flex flex-col items-center justify-start bg-gray-50">
+				<h1 className="text-3xl text-center mt-10 text-midnight tracking-wide font-semibold">
+					CREATE ACCOUNT
+				</h1>
+				<form
+					onSubmit={(event) => {
+						event.preventDefault();
+					}}
+					className="mt-10">
+					<div className="flex bg-light px-2 py-1 items-center rounded-md gap-2 mb-4 focus-within:shadow-md">
+						<label htmlFor="Nome">
+							<FaUser size={18} className="text-midnight" />
+						</label>
+						<input
+							onChange={(event) => setNameInput(event.target.value)}
+							name="Nome"
+							autoComplete="off"
+							placeholder="* Nome Completo"
+							required
+							type="text"
+							minLength={5}
+							maxLength={40}
+							className="bg-transparent flex-1 focus:outline-none"
+						/>
+					</div>
+					<div className="flex bg-light px-2 py-1 items-center rounded-md gap-2 mb-4 focus-within:shadow-md">
+						<label htmlFor="Email">
+							<FaEnvelope size={18} className="text-midnight" />
+						</label>
+						<input
+							onChange={(event) => setSetEmailInput(event.target.value)}
+							name="Email"
+							placeholder="* Email"
+							autoComplete
+							type="email"
+							required
+							className="bg-transparent flex-1 focus:outline-none"
+						/>
+					</div>
+					<div className="flex bg-light px-2 py-1 items-center rounded-md gap-2 focus-within:shadow-md">
+						<label htmlFor="Senha">
+							<FaLock size={18} className="text-midnight" />
+						</label>
+						<input
+							onChange={(event) => setPasswordInput(event.target.value)}
+							name="Senha"
+							required
+							type="password"
+							autoComplete="off"
+							placeholder="* Senha"
+							className="bg-transparent flex-1 focus:outline-none"
+						/>
+					</div>
+					<span className="flex w-full justify-center my-6">OU</span>
+					<button
+						type="submit"
+						className="bg-midnight transition-all hover:bg-blue-800 text-light w-full uppercase flex justify-center items-center mb-5 gap-3 p-2 rounded-md">
+						<FaSignInAlt />
+						Acessar
+					</button>
+					<button className="bg-light text-midnight transition-all hover:bg-zinc-400 w-full uppercase flex justify-center items-center gap-3 p-2 rounded-md">
+						<FaGoogle />
+						Continue with Google
+					</button>
+				</form>
+			</div>
+		</main>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				midnight: "#121063",
+				light: "#E9E9E9",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animated")],
+};
